9e41c72accf97cd0962251d4b1a0f1c7e147b278
MS_Home <anydog.official@gmail.com>
2023-12-15 09:23:04 +0900

    FEAT : nav.css 수정

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,31 +7,33 @@ const Nav = () => {
   return (
     <>
       <div className="navbar">
-        <div className="navbar_Logo_Container">
-          <Link className="navbar_Logo" to={"/"}>
-            <img
-              className="navbar_Logo_Img"
-              alt="로고"
-              src={process.env.PUBLIC_URL + `/assets/GM_Logo_Black.png`}
-            ></img>
-          </Link>
-        </div>
-        <div className="navbar_Menu_Container">
-          <Link className="navbar_Menu" to={"/project"}>
-            프로젝트
-          </Link>
-          <Link className="navbar_Menu" to={"/review"}>
-            활동후기
-          </Link>
-          <Link className="navbar_Menu" to={"/hof"}>
-            명예의 전당
-          </Link>
-          <Link className="navbar_Menu" to={"/recruit"}>
-            모집
-          </Link>
-          <Link className="navbar_Menu" to={"/inquiry"}>
-            문의
-          </Link>
+        <div className="navbar_Container">
+          <div className="navbar_Logo_Container">
+            <Link className="navbar_Logo" to={"/"}>
+              <img
+                className="navbar_Logo_Img"
+                alt="로고"
+                src={process.env.PUBLIC_URL + `/assets/GM_Logo_Black.png`}
+              ></img>
+            </Link>
+          </div>
+          <div className="navbar_Menu_Container">
+            <Link className="navbar_Menu" to={"/project"}>
+              프로젝트
+            </Link>
+            <Link className="navbar_Menu" to={"/review"}>
+              활동후기
+            </Link>
+            <Link className="navbar_Menu" to={"/hof"}>
+              명예의 전당
+            </Link>
+            <Link className="navbar_Menu" to={"/recruit"}>
+              모집
+            </Link>
+            <Link className="navbar_Menu" to={"/inquiry"}>
+              문의
+            </Link>
+          </div>
         </div>
       </div>
     </>
