6f90ffd31aeaff6352692e702505ed941991f192
MS_Home <anydog.official@gmail.com>
2024-01-06 13:46:08 +0900

    Complete Logoslider

--- src/components/LogoSlider.js
+++ src/components/LogoSlider.js
@@ -5,168 +5,32 @@ const LogoSlider = () => {
     <div className="logoslider">
       <div className="animation_list animation">
         <div className="logoslider-container">
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/cookapps_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/devsisters_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/ncsoft_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/neople_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/netmarble_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/nexon_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/npixel_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/pearlabyss_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/smilegate_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/supercent_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/treeplla_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/wemadeplay_logo.png`} alt="Subtitle_Star"></img>
         </div>
         <div className="logoslider-container">
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
-          <div className="img-box">
-            <img
-              src={
-                process.env.PUBLIC_URL + `/assets/home/Navbar_GM_Logo_Black.png`
-              }
-              alt="Subtitle_Star"
-            ></img>
-          </div>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/cookapps_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/devsisters_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/ncsoft_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/neople_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/netmarble_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/nexon_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/npixel_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/pearlabyss_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/smilegate_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/supercent_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/treeplla_logo.png`} alt="Subtitle_Star"></img>
+          <img className="img-box" src={process.env.PUBLIC_URL + `/assets/home/logo/wemadeplay_logo.png`} alt="Subtitle_Star"></img>
         </div>
       </div>
     </div>
