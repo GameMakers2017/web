7dcfeeed68a115be6822628a7f526ca7c8a659ce
GameMakers <35674072+GameMakers2017@users.noreply.github.com>
2024-01-11 16:28:28 +0900

    Add <div> * 2

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,6 +8,9 @@ const Home = () => {
   return (
     <>
       <main>
+        <div>
+          <div></div>
+        </div>
         <section id="home-main">
           <div className="home-main">
             <div className="home-main-container">
